0496a6fbb346e46f495135223bed630af94f871d
Tre <mitch3moore@gmail.com>
2021-01-19 21:15:28 -0500

    Fixed dotenv issues

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,10 @@
 import express = require('express');
-
 import path = require('path');
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
-const PORT: number = parseInt(process.env.PORT, 10) || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.status(200);
